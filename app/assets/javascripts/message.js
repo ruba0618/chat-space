db03403a1484d5217b368a54a08a1913c6926732
ruba0618 <ruba061806180618@gmail.com>
2019-12-15 16:38:42 +0900

    ChatSpaceの投稿機能実装

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,66 @@
+$(function(){
+  function buildHTML(message){
+    // 「もしメッセージに画像が含まれていたら」という条件式
+    if (message.image) {
+      var html = `<div class="message">
+      <div class="upper-message">
+      <div class="upper-message__user-name">
+      
+      </div>
+      <div class="upper-message__date">
+      
+      </div>
+      </div>
+      <div class="lower-message">
+      <p class="lower-message__content">
+      
+      </p>
+      <img class="lower-message__image" src="/uploads/message/image/21/arrow_top.png" alt="Arrow top">
+      </div>
+      </div>`
+    }
+     else {
+      var html = `<div class="message">
+      <div class="upper-message">
+      <div class="upper-message__user-name">
+      
+      </div>
+      <div class="upper-message__date">
+      
+      </div>
+      </div>
+      <div class="lower-message">
+      <p class="lower-message__content">
+      
+      </p>
+      </div>
+      </div>`//メッセージに画像が含まれない場合のHTMLを作る
+    }
+    return html
+  }
+  
+  console.log("js読み込み")
+  $(".new_message").on("submit", function(e){
+    e.preventDefault()
+    console.log("イベント発火")
+    var url = $("#new_message").attr("action");
+    formData = new FormData(this);
+    $.ajax({
+      url:  url,  //同期通信でいう『パス』
+      type: 'POST',  //同期通信でいう『HTTPメソッド』
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.main_chat__group').append(html);
+      $('.main_chat__group').animate({ scrollTop: $('.main_chat__group')[0].scrollHeight});
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+  });
+  })
+})
+
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,5 @@
+json.content @message.content
+json.user_name @message.user.name
+json.created_at @message.created_at
+json.id @message.id
+json.image @message.image

8d70ec1f2eda8ac5a1fbd2f47c2e993fe45fb942
ruba0618 <ruba061806180618@gmail.com>
2019-12-16 12:04:48 +0900

    完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
 
   def index
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def new
@@ -10,11 +16,11 @@ class UsersController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    if @group.save
-      redirect_to root_path, notice: 'グループを作成しました'
-    else
-      render :new
-    end
+      if @group.save
+        redirect_to root_path, notice: 'グループを作成しました'
+      else
+        render :new
+      end
   end
 
   private
